13cabf31ffa2b3749564333f23900ea3d6d0e803
ToddLa <ToddLa@HotMail.com>
2022-02-07 14:26:59 -0800

    added release param to make-ios.sh

--- make-ios.sh
+++ make-ios.sh
@@ -7,15 +7,20 @@
 ##
 ## target is one of the following
 ##      <blank>                 build libmame-ios.a for iOS
+##      ios                     build libmame-ios.a for iOS
 ##      ios-simulator           build libmame-ios.a for iOS Simulator
 ##      tvos                    build libmame-tvos.a for tvOS
 ##      tvos-simulator          build libmame-tvos.a for tvOS Simulator
 ##      mac-catalyst            build libmame-mac.a for macOS Catalyst
 ##      mac-catalyst-arm64      build libmame-mac-arm64.a for macOS Catalyst
 ##      mac-catalyst-x86_64     build libmame-mac-x86_64.a for macOS Catalyst
+##      mame                    build mame
+##      all                     build ios, tvos, and mac-catalyst
+##      clean                   clean all
+##      release                 build all and gzip
 ##
 
-VERSION_MIN=12.4
+VERSION_MIN=13.4
 
 ## iOS is the default
 export BUILDDIR=build-ios
@@ -54,7 +59,7 @@ if [ "$1" == "mac-catalyst-arm64" ]; then
     LIBMAME=libmame-mac-arm64
     SDK=`xcodebuild -version -sdk macosx Path`
     export BUILDDIR=build-mac-arm64
-    export ARCHOPTS="-arch arm64 -isysroot $SDK -iframework $SDK/System/iOSSupport/System/Library/Frameworks -miphoneos-version-min=$VERSION_MIN -target arm64-apple-ios13.6-macabi"
+    export ARCHOPTS="-arch arm64 -isysroot $SDK -iframework $SDK/System/iOSSupport/System/Library/Frameworks -miphoneos-version-min=$VERSION_MIN -target arm64-apple-ios$VERSION_MIN-macabi"
     shift
 fi
 
@@ -63,7 +68,7 @@ if [ "$1" == "mac-catalyst-x86_64" ]; then
     LIBMAME=libmame-mac-x86_64
     SDK="`xcodebuild -version -sdk macosx Path`"
     export BUILDDIR=build-mac-x86_64
-    export ARCHOPTS="-arch x86_64 -isysroot $SDK -iframework $SDK/System/iOSSupport/System/Library/Frameworks -miphoneos-version-min=$VERSION_MIN -target x86_64-apple-ios13.6-macabi"
+    export ARCHOPTS="-arch x86_64 -isysroot $SDK -iframework $SDK/System/iOSSupport/System/Library/Frameworks -miphoneos-version-min=$VERSION_MIN -target x86_64-apple-ios$VERSION_MIN-macabi"
     shift
 fi
 
@@ -82,11 +87,64 @@ if [ "$1" == "mac-catalyst" ]; then
     exit
 fi
 
+if [ "$1" == "mame" ]; then
+    shift
+    make USE_BUNDLED_LIB_SDL2=1 $@ -j`sysctl -n hw.logicalcpu`
+    exit
+fi
+
 if [ "$1" == "all" ]; then
     shift
-    $0 $@ || exit -1
+    $0 ios $@ || exit -1
     $0 tvos $@ || exit -1
     $0 mac-catalyst $@ || exit -1
+    exit
+fi
+
+if [ "$1" == "simulator" ]; then
+    shift
+    $0 ios-simulator $@ || exit -1
+    $0 tvos-simulator $@ || exit -1
+    exit
+fi
+
+if [ "$1" == "clean" ]; then
+    shift
+    $0 all clean || exit -1
+    rm libmame-ios.a
+    rm libmame-tvos.a
+    rm libmame-mac.a
+    rm *.gz
+    exit
+fi
+
+if [ "$1" == "release" ]; then
+    shift
+    
+    ## build
+    echo Build
+    $0 all || exit -1
+    
+    ## hash.zip
+    echo Hash
+    [ ! -f hash.zip ] || rm hash.zip
+    cd hash
+    zip --quiet --no-dir-entries --recurse-paths ../hash.zip *
+    cd ..
+    
+    ## plugins.zip
+    echo Plugins
+    [ ! -f plugins.zip ] || rm plugins.zip
+    cd plugins
+    zip --quiet --no-dir-entries --recurse-paths ../plugins.zip *
+    cd ..
+    
+    ## gzip
+    echo Compressing libraries
+    gzip --keep --force libmame-ios.a || exit -1
+    gzip --keep --force libmame-tvos.a || exit -1
+    gzip --keep --force libmame-mac.a || exit -1
+
     exit
 fi
 
